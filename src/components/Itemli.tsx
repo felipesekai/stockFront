49bbd3c07fa78dbc65e9a89f6a6088f4c878b280
felipe sekai <felippe10_@hotmail.com>
2023-03-02 12:25:00 -0300

    ajueste deploy

--- src/components/Itemli.tsx
+++ src/components/Itemli.tsx
@@ -1,33 +1,33 @@
-import {LiHTMLAttributes, useEffect, useState} from "react";
-import {bgColor} from "../utils/Colors";
-import {Products} from "../utils/@Types";
-import {formatCurrency} from "../utils/converter";
+import { LiHTMLAttributes, useEffect, useState } from "react";
+import { bgColor } from "../utils/Colors";
+import { Products } from "../utils/@Types";
+import { formatCurrency } from "../utils/converter";
 
 interface props extends LiHTMLAttributes<HTMLLIElement> {
     item: Products;
     itemSelectId?: string
-    onSelectChange: (item: Products)=>void
+    onSelectChange: (item: Products) => void
 }
-export const Itemli = ({item, onSelectChange, itemSelectId, ...rest}: props)=>{
+export const Itemli = ({ item, onSelectChange, itemSelectId, ...rest }: props) => {
     const [select, setSelect] = useState(false)
-    const handleSelect = ()=>{
+    const handleSelect = () => {
         onSelectChange(item)
     }
 
-    useEffect(()=>{
-        if(itemSelectId === item.id){
+    useEffect(() => {
+        if (itemSelectId === item.id) {
             setSelect(true)
-        }else{
+        } else {
             setSelect(false)
 
         }
-    },[itemSelectId])
+    }, [item.id, itemSelectId])
 
     return (
         <li
             {...rest}
-        onClick={handleSelect}
-        className="
+            onClick={handleSelect}
+            className="
         py-2
         flex
         border-b-gray-light
@@ -38,16 +38,16 @@ export const Itemli = ({item, onSelectChange, itemSelectId, ...rest}: props)=>{
         px-2
         justify-between
         "
-        style={{backgroundColor: select? bgColor.bgPrimary : '', borderWidth: select ? '1px': '' }}
+            style={{ backgroundColor: select ? bgColor.bgPrimary : '', borderWidth: select ? '1px' : '' }}
         >
             <div className={'flex-[2]'}>
-            {item.name}
+                {item.name}
             </div>
             <div className={'flex-1'}>
-            {"Qtd: "+item.quantity}
+                {"Qtd: " + item.quantity}
             </div>
             <div >
-            {"Preço: "+ formatCurrency(Number(item.price))}
+                {"Preço: " + formatCurrency(Number(item.price))}
             </div>
         </li>
     );
